0203da1bde941a7d0b9a3edf33b6f4c31cb871b5
Lucas Protestato Tavares <101677196+LucasTavare@users.noreply.github.com>
2022-05-13 21:17:25 -0300

    site finalizado

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+
+// embarque nesse carrossel
 const myCarousel = new Carousel(document.querySelector("#myCarousel"), {
     // Your options go here
   });
+
+// função que oculta e desoculta o menu
+
+const exibeMenu = () => {
+  $('.oculta-navbar').toggle()
+}
